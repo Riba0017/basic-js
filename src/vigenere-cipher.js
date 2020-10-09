09828646e46c17f47d5cd7f26e9b868608e7c4cc
Kate <Riba0017@yandex.ru>
2020-10-09 23:06:36 +0300

    feat: add last solution, finally

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode) {
+    this.mode = mode !== false ? "direct" : "reverse";
+    this.abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.finishString = {
+      direct: i => i,
+      reverse: i =>
+          i
+              .split("")
+              .reverse()
+              .join("")
+    };
+  }
+
+  normalizeKey(message, key) {
+    while (message.length > key.length) {
+      key += key;
+    }
+    for (let i = 0; i < message.length; i++) {
+      if (this.abc.indexOf(message.charAt(i)) === -1) {
+        key = key.slice(0, i) + message.charAt(i) + key.slice(i);
+      }
+    }
+    return key;
+  }
+
+  createString(message, key, flag) {
+    message = message.toUpperCase();
+    key = this.normalizeKey(message, key.toUpperCase());
+    let res = "";
+    for (let i = 0; i < message.length; i++) {
+      if (this.abc.indexOf(message.charAt(i)) === -1) {
+        res += message.charAt(i);
+      } else {
+        if (flag === true) {
+          res += this.abc.charAt(
+              (this.abc.indexOf(message.charAt(i)) +
+                  this.abc.indexOf(key.charAt(i))) %
+              this.abc.length
+          );
+        } else {
+          res += this.abc.charAt(
+              (this.abc.indexOf(message.charAt(i)) -
+                  this.abc.indexOf(key.charAt(i)) +
+                  this.abc.length) %
+              this.abc.length
+          );
+        }
+      }
+    }
+    return this.finishString[this.mode](res);
+  }
+
+  encrypt(...params) {
+    if (params.length !== 2) throw Error();
+    return this.createString(params[0], params[1], true);
+  }
+
+  decrypt(...params) {
+    if (params.length !== 2) throw Error();
+    return this.createString(params[0], params[1], false);
   }
 }
 
